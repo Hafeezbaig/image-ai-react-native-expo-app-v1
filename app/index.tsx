b7c02f2c4d012dfd0e8961dba2934bbc1938f929
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-04-29 21:07:15 +0530

    index updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView, StatusBar } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#0A0A0A" }}>
+      <StatusBar barStyle="light-content" backgroundColor="#0A0A0A" />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingHorizontal: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: "bold",
+            color: "#FFFFFF",
+            textAlign: "center",
+          }}
+        >
+          👋 Welcome!
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#BBBBBB",
+            marginTop: 10,
+            textAlign: "center",
+          }}
+        >
+          Generate Image at one place.
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
